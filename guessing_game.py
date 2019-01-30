f1684bd9bd19b95884540e3026eb98cc0fd1d127
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-01-30 16:14:15 -0500

    basic game running. still needing to add try blocks

--- guessing_game.py
+++ guessing_game.py
@@ -2,19 +2,12 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
 
+"""Psuedo-code Hints
 
-def start_game():
-    """Psuedo-code Hints
-    
     When the program starts, we want to:
     ------------------------------------
     1. Display an intro/welcome message to the player.
@@ -22,16 +15,55 @@ def start_game():
     3. Continuously prompt the player for a guess.
       a. If the guess greater than the solution, display to the player "It's lower".
       b. If the guess is less than the solution, display to the player "It's higher".
-    
+
     4. Once the guess is correct, stop looping, inform the user they "Got it"
          and show how many attempts it took them to get the correct number.
     5. Let the player know the game is ending, or something that indicates the game is over.
-    
+
     ( You can add more features/enhancements if you'd like to. )
     """
-    # write your code inside this function.
+# write your code inside this function.
+solution = random.randint(1, 10)
+
+guesses = []
+
+print("""
+-----------------------------
+Welcome to the Guessing Game!
+-----------------------------
+""")
+
+# def start_game():
+player_name = input("Please enter your name:  ")
+
+print("""
+Good luck {}!
+Remember to have fun!!
+""".format(player_name))
+
+start = input("Are you ready to begin? (Y)es or (N)o:  ")
 
 
-if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
-    start_game()
+while start.lower() == "y":
+    guess = int(input("What number would you like to try (1-10):  "))
+    if guess > solution:
+        guesses.append(guess)
+        print("The magic number is lower then {}.".format(guess))
+        continue
+    elif guess < solution:
+        guesses.append(guess)
+        print("The magic number is higher then {}.".format(guess))
+        continue
+    elif guess == solution:
+        guesses.append(guess)
+        if len(guesses) == 1:
+            print("Wow!! Lucky guess. You have found the magic number on your first try!!!")
+            break
+        else:
+            print("Good game. You have found the magic number in {} tries!!!".format(len(guesses)))
+            break
+print("Have a good day {} and come back and play anytime.".format(player_name))
+
+# if __name__ == '__main__':
+#     # Kick off the program by calling the start_game function.
+#     start_game()
